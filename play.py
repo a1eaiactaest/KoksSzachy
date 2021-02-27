abebf5c78fefe850b15bcc61a178964a3e124376
piotrek-desktop <piotrrybiec28@gmail.com>
2021-02-27 10:18:35 +0100

    finish state

--- play.py
+++ play.py
@@ -3,6 +3,7 @@
 import os
 import chess
 import chess.svg
+import traceback
 import time
 from state import State
 
@@ -81,9 +82,6 @@ class Valuator():
     self.board.set_fen(fen)
     self.depth = 0 
 
-  def reset(self):
-    self.count = 0
-
   def value(self):
     val = 0
     for i in range(1,7):
@@ -93,7 +91,6 @@ class Valuator():
 
 
 
-#//////////////
 s = State() # plansza itd
 
 from flask import Flask, Response, request
@@ -102,7 +99,60 @@ app = Flask(__name__)
 @app.route("/")
 def hello():
   ret = open('index.html').read()
-  return ret.replace('start', s.board.fen())
+  return ret.replace('start', s.board.fen()) # zastap pozycje w configu chassboard z obecnym fen stringiem
+
+def ai_move(s, v):
+  pass
+
+@app.route("/move")
+def move():
+  if not s.board.is_game_over():
+    move = request.args.get('move', default="") # przejmij wartosc move z index.html
+    if move is not None and move != "":
+      print("your move: ", move)
+      try:
+        s.board.push_san(move)
+        ai_move(s, v) # zrobic funkcje ai
+      except Exception:
+        # https://docs.python.org/3/library/traceback.html#traceback.print_exc
+        # https://docs.python.org/3/library/traceback.html#traceback.print_exception
+        traceback.print_exc() # w skrocie, wypisz bledy ze stacka
+      response = app.response_class(      
+        response = s.board.fen(),
+        status = 200
+      )
+      return response
+  else:
+    print("GAME OVER")
+    response = app.response_class(
+      response="game over",
+      status=200
+    )
+    return response
+  return hello() # caly czas czytaj index.html
+
+@app.route("/move_cords")
+def move_cords():
+  if not s.board.is_game_over():
+    source = int(request.args.get('from', default=''))
+    target = int(request.args.get('to', default=''))
+    promo = True if request.args.get('promo', default='') == 'true' else False
+
+    move = s.bard.san(chess.Move(source, target, pormotion=chess.QUEEN if promo else None)) # auto promocja do hetmana
+
+    if move is not Noen and move !='':
+      print('your move: ', move)
+      try:
+        s.board.push_san(move)
+        ai_move(s, V)
+      except Exception:
+        traceback.print_exc()
+    response = app.response_class(
+      response = s.board.fen(),
+      status = 200
+    )
+    return response
 
 if __name__ == "__main__":
+  s = State()
   app.run(debug=True)
--- state.py
+++ state.py
@@ -8,12 +8,13 @@ class State(object):
     else:
       self.board = board
 
+
   def key(self):
     return (self.board.board_fen(), self.board.turn, self.board.castling_rigths, self.board.ep_square)    
 
   def edges(self):
     return list(self.board.legal_moves)
 
+
 if __name__ == "__main__":
   s = State()
-  print(s.board.board_fen())
